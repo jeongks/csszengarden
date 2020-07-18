1863e36ce91cdb6022bf980c43470a414205df54
Kisuk Jeong <jeongks@users.noreply.github.com>
2020-07-18 13:01:14 +0900

    Delete archive.js

--- archive.js
+++
@@ -1,8 +0,0 @@
-const container = document.createElement("div");
-container.id = "navbtncontainer"
-const foldbtn = document.createElement("span");
-foldbtn.id = "navbtn";
-
-container.appendChild(foldbtn);
-document.getElementById("design-archives").appendChild(container);
-
