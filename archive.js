5097ef67c430f72e1e6b487ad848aae7a32cfc40
jeongks <kisuk.jeong.lds@gmail.com>
2020-07-18 12:58:18 +0900

    final

---
+++ archive.js
@@ -0,0 +1,8 @@
+const container = document.createElement("div");
+container.id = "navbtncontainer"
+const foldbtn = document.createElement("span");
+foldbtn.id = "navbtn";
+
+container.appendChild(foldbtn);
+document.getElementById("design-archives").appendChild(container);
+
